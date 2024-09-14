c92748ee590fa7c922aeffbf897fafa04a1b740f
Yusuke <galapon.player@gmail.com>
2024-09-15 00:43:16 +0900

    log

--- backend/src/lambda_handler/api_for_public/main.go
+++ backend/src/lambda_handler/api_for_public/main.go
@@ -1,40 +1,42 @@
 package main
 
 import (
-    "log"
-    "context"
+	"context"
+	"log"
 
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-    "github.com/gin-gonic/gin"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
 var ginLambda *ginadapter.GinLambda
 
 func init() {
-    // stdout and stderr are sent to AWS CloudWatch Logs
-    log.Printf("Gin cold start")
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "CICD Success!",
-        })
-    })
-    r.GET("/hello", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Hello World!",
-        })
-    })
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Gin cold start")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "CICD Success!",
+		})
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
 
-    ginLambda = ginadapter.New(r)
+	ginLambda = ginadapter.New(r)
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    // If no name is provided in the HTTP request body, throw an error
-    return ginLambda.ProxyWithContext(ctx, req)
+	// If no name is provided in the HTTP request body, throw an error
+	log.Printf("req.Path: " + req.Path)
+	log.Printf("req: %v", req)
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-    lambda.Start(Handler)
+	lambda.Start(Handler)
 }
