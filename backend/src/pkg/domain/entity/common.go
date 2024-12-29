a0481d5f802dfeee7202827e39039aeb606e1406
Yusuke <galapon.player@gmail.com>
2024-12-29 18:26:05 +0900

    deletedAtをNULL許容に

--- backend/src/pkg/domain/entity/common.go
+++ backend/src/pkg/domain/entity/common.go
@@ -3,7 +3,7 @@ package entity
 import "time"
 
 type CommonDBAttributes struct {
-	CreatedAt  time.Time `json:"created_at" gorm:"autoCreateTime"`
-	ModifiedAt time.Time `json:"modified_at" gorm:"autoUpdateTime"`
-	DeletedAt  time.Time `json:"deleted_at"`
+	CreatedAt  time.Time  `json:"created_at" gorm:"autoCreateTime"`
+	ModifiedAt time.Time  `json:"modified_at" gorm:"autoUpdateTime"`
+	DeletedAt  *time.Time `json:"deleted_at" gorm:"index;default:null"`
 }
